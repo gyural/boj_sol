b7d8b4c0e92a3eff6e2c657a646babd974bfbe5f
GyuSeongLim <100032171+gyural@users.noreply.github.com>
2023-03-31 16:04:33 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,13 +1,4 @@
 function solution(my_string, letter) {
-    var answer = '';
-    tmp_list = my_string.split('')
-    N = tmp_list.length
-    for (i = 0; i < N; i++){
-        c = tmp_list.pop()
-        if (c != letter){
-            tmp_list.unshift(c)
-        }
-    }
-    answer = tmp_list.join('')
-    return answer;
+    
+    return my_string.split(letter).join('');
 }
