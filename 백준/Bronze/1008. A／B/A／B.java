8a4ccd74fcf5b8fedd7c0e021cd3b5c5fb221bad
Lim, gyuseong <dla93190@naver.com>
2025-04-19 20:08:49 +0900

    [Bronze V] Title: A/B, Time: 184 ms, Memory: 17820 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+    Scanner sn = new Scanner(System.in);
+
+    int a = sn.nextInt();
+    int b = sn.nextInt();
+
+    System.out.println(a*1.0/b);
+
+  }
+}
