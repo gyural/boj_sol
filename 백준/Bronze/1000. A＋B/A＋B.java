94f39e1694a5aff744101bfe7cc32d886f8f9392
Lim, gyuseong <dla93190@naver.com>
2025-04-19 19:55:30 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+
+    Scanner sn = new Scanner(System.in);
+
+    int a = sn.nextInt();
+    int b = sn.nextInt();
+
+      
+    System.out.println(a+b);
+    
+    
+    
+  }
+}
