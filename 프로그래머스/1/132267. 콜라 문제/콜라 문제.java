1da90ed9fe3e5a348bb5dc1915f99127e54cf10a
Lim, gyuseong <dla93190@naver.com>
2025-12-10 19:21:48 +0900

    [level 1] Title: 콜라 문제, Time: 0.07 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -2,17 +2,22 @@ class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
         
-        while (n >= a){
-            // 1. 여분 남기고
-            int spare = n % a;
+        int curBottleNum = n;
+        int collectNum = a;
+        int bonusNum = b;
+        
+        while(curBottleNum >= collectNum) {
             
-            // 2. 새롭게 받기
-            int newNum = n / a * b;
-            answer += newNum;
+            int divNum = curBottleNum / collectNum;
+            int modNum = curBottleNum % collectNum;
+            int newBottle = divNum * bonusNum;
             
-            n = newNum + spare;
             
+            answer+= newBottle;
+            
+            curBottleNum = newBottle + modNum;
         }
+        
         return answer;
     }
 }
