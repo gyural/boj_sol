bd89680b26f8e9332047b98ddb4c5718406b4266
Lim, gyuseong <dla93190@naver.com>
2025-12-10 21:38:19 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 1.47 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,27 +1,25 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 class Solution {
     public String[] solution(String[] strings, int n) {
-        String[] answer = new String[strings.length];
+        int size = strings.length;
+        String[] answer = new String[size];
         
-        List<String> stringList = new ArrayList();
+        List<String> arr = new ArrayList<>();
+        for (String s : strings) {
+            arr.add(s);
+        }
+        
+        arr.sort((a,b) -> a.compareTo(b));
+        arr.sort((a,b) -> {
+            return a.charAt(n) - b.charAt(n);
+        });
         
         
-        for (String s: strings) stringList.add(s);
-        
-        stringList.sort(Comparator.naturalOrder());
-        stringList.sort((a,b) -> a.charAt(n) - b.charAt(n));
-        
-        for (int i =0; i< stringList.size(); i++) answer[i] = stringList.get(i);
+        for (int i=0; i<size; i++) {
+            answer[i] = arr.get(i);
+        }
         
         return answer;
     }
 }
-
-
-// 1. List의 string일때 정렬 역정렬이 가능해야함 사전순...
-// 2. 일반 array일때 역사전순 정렬이 가능해야함
